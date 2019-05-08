44cce5bd83482ae73f7606f618e1b680e2bd8a45
Gagandeep singh <g.deepsingh1@gmail.com>
2019-05-08 15:13:46 +0530

    interfaces

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from "@angular/core";
+import { IProduct } from "./product";
 
 @Component({
   selector: "pm-products",
@@ -10,7 +11,7 @@ export class ProductListComponent {
   imageHeight = 2;
   showImage: boolean = false;
   search: String = "search";
-  products: any[] = [
+  products: IProduct[] = [
     {
       productId: 2,
       productName: "Garden Cart",
