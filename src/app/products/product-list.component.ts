b1da7c3d22fa964f11ee4fd50d1af025c14f3df6
Gagandeep singh <g.deepsingh1@gmail.com>
2019-05-09 14:57:15 +0530

    filter product

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -11,7 +11,9 @@ export class ProductListComponent implements OnInit {
   imageWidth = 50;
   imageHeight = 2;
   showImage: boolean = false;
-  search: String = "search";
+
+  filteredProducts: IProduct[] = [];
+
   products: IProduct[] = [
     {
       productId: 2,
@@ -36,6 +38,22 @@ export class ProductListComponent implements OnInit {
         "https://openclipart.org/image/300px/svg_to_png/73/rejon_Hammer.png"
     }
   ];
+  _listFilter: string;
+  set listFilter(value: string) {
+    this._listFilter = value;
+    this.filteredProducts = this.products
+      ? this.performFilter(value)
+      : this.products;
+  }
+
+  get listFilter() {
+    return this._listFilter;
+  }
+
+  constructor() {
+    this.filteredProducts = this.products;
+    this.listFilter = "cart";
+  }
 
   ngOnInit() {
     console.log("in init");
@@ -43,5 +61,13 @@ export class ProductListComponent implements OnInit {
 
   toggleImage(): void {
     this.showImage = !this.showImage;
+  }
+
+  performFilter(value: string): IProduct[] {
+    value = value.toLocaleLowerCase();
+    return this.products.filter(
+      (product: IProduct) =>
+        product.productName.toLocaleLowerCase().indexOf(value) !== -1
+    );
   }
 }
