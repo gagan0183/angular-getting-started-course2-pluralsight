a99a6c7e36dd5712c384ddc32a8b9b1783e533cc
Gagandeep singh <g.deepsingh1@gmail.com>
2019-05-09 17:07:37 +0530

    nested component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,15 @@ import { FormsModule } from "@angular/forms";
 import { AppComponent } from "./app.component";
 import { ProductListComponent } from "./products/product-list.component";
 import { ConvertToSpaces } from "./pipes/convert-to-spaces.pipe";
+import { StarComponent } from "./star/star.component";
 
 @NgModule({
-  declarations: [AppComponent, ProductListComponent, ConvertToSpaces],
+  declarations: [
+    AppComponent,
+    ProductListComponent,
+    ConvertToSpaces,
+    StarComponent
+  ],
   imports: [BrowserModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent]
