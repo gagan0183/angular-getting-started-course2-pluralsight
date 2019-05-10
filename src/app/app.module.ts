bd894c49db46e6e852e4f56908ded0b635e4da60
Gagandeep singh <g.deepsingh1@gmail.com>
2019-05-10 18:49:12 +0530

    routes integration

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 import { FormsModule } from "@angular/forms";
 import { HttpClientModule } from "@angular/common/http";
+import { RouterModule } from "@angular/router";
 
 import { AppComponent } from "./app.component";
 import { ProductListComponent } from "./products/product-list.component";
@@ -20,7 +21,18 @@ import { WelcomeComponent } from "./welcome/welcome.component";
     ProductDetailComponent,
     WelcomeComponent
   ],
-  imports: [BrowserModule, FormsModule, HttpClientModule],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpClientModule,
+    RouterModule.forRoot([
+      { path: "products", component: ProductListComponent },
+      { path: "product/:id", component: ProductDetailComponent },
+      { path: "welcome", component: WelcomeComponent },
+      { path: "", redirectTo: "welcome", pathMatch: "full" },
+      { path: "**", redirectTo: "welcome", pathMatch: "full" }
+    ])
+  ],
   providers: [ProductService],
   bootstrap: [AppComponent]
 })
